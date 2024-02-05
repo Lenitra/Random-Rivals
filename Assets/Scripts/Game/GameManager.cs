665fff309e70c253853e809dbfecf373ffdd1c49
TauMah <lenitramc@gmail.com>
2024-02-05 12:36:41 +0100

    Restructure globale du code Effet de surbrillance sur les cases sur lesquelles le joueur peut cliquer Panneau de débug pour voir les faces des différents dés sur le terrain

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -5,174 +5,124 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private View view; // Référence à la vue
+    // [SerializeField] private View view; // Référence à la vue
     [SerializeField] private GameObject dicePrefab; // Liste des dés
+    [SerializeField] private GameObject[] tilePrefabs;
+    [SerializeField] private GameObject mapContainer;
+    [SerializeField] private GameObject highlightTilesContainer;
+    [SerializeField] private GameObject tileSurbrillance;
+    [SerializeField] private Transform camera;
+    [SerializeField] private Text endText;
+    [SerializeField] private GameObject endPanel;
+    [SerializeField] private GameObject detailPanel;
+
     
     public int nbIA = 3; // Nombre d'IA
     public int nbPlayer = 2; // Nombre dés joueurs
 
     // Liste des joueurs
-    public List<Dice> dices = new List<Dice>();
-    public int playerTurn = 0;
+    private List<Dice> dices = new List<Dice>();
+    private int playerTurn = 0;
     private int[,] map = new int[10,10];
-
     private bool endGame = false;
-    public Transform camera;
-    public Text endText;
-    public GameObject endPanel;
+    
 
-
+    void Start()
+    {
+        initGame();
+    }
 
     // Quand le joueur charge une partie (initialisation)
     public void initGame(){
         // Générer la map
-        GenerateMap(1);
+        GenerateMap();
         // Générer les dés
         GenerateDice();
         camera.position = new Vector3(map.GetLength(0)/2, map.GetLength(1)/2, -10);
     }
 
 
-    // Génère les dés
-    // Place les dés sur la map
-    // Les affiche dans la vue
-    private void GenerateDice(){
-
+    private void GenerateDice() {
         // Génère les joueurs
-        for (int i = 0; i < nbPlayer; i++)
-        {
-            // Créer un dé
-            GameObject dice = Instantiate(dicePrefab, new Vector3(0, 0, 0), Quaternion.identity);
-
-            // Changer le nom du dé
-            dice.name = "P_" + i;
-
-            // Trouver une case vide non occupée
-            int x = 0, y = 0;
-            bool isOccupied;
-            do
-            {
-                isOccupied = false;
-                x = Random.Range(0, map.GetLength(0));
-                y = Random.Range(0, map.GetLength(1));
-                
-                // Vérifier si la case est vide et non occupée
-                if (map[x, y] != 1)
-                {
-                    isOccupied = true;
-                    continue;
-                }
-
-                // Vérifier si un autre dé occupe déjà cette position
-                foreach (Dice d in dices)
-                {
-                    if (d.posX == x && d.posY == y)
-                    {
-                        isOccupied = true;
-                        break;
-                    }
-                }
-            } while (isOccupied);
-
-            // gérer les attributs de la classe Dice
-            dice.GetComponent<Dice>().posX = x;
-            dice.GetComponent<Dice>().posY = y;       
-            dice.GetComponent<Dice>().isAI = false;     
-
-            // Ajouter le dé à la liste des joueurs
-            dices.Add(dice.GetComponent<Dice>());
-        }
+        GenerateDiceForEntities(nbPlayer, false);
 
         // Génère les IA
-        for (int i = 0; i < nbIA; i++)
-        {
+        GenerateDiceForEntities(nbIA, true);
+    }
+
+    private void GenerateDiceForEntities(int nbEntities, bool isAI) {
+        for (int i = 0; i < nbEntities; i++) {
             // Créer un dé
             GameObject dice = Instantiate(dicePrefab, new Vector3(0, 0, 0), Quaternion.identity);
 
-            // Changer le nom du dé
-            dice.name = "AI_" + i;
+            // Changer le nom du dé en fonction du type (Joueur ou IA)
+            dice.name = isAI ? $"AI_{i}" : $"P_{i}";
 
             // Trouver une case vide non occupée
-            int x = 0, y = 0;
-            bool isOccupied;
-            do
-            {
-                isOccupied = false;
-                x = Random.Range(0, map.GetLength(0));
-                y = Random.Range(0, map.GetLength(1));
-                
-                // Vérifier si la case est vide et non occupée
-                if (map[x, y] != 1)
-                {
-                    isOccupied = true;
-                    continue;
-                }
+            (int x, int y) = FindEmptyPosition();
 
-                // Vérifier si un autre dé occupe déjà cette position
-                foreach (Dice d in dices)
-                {
-                    if (d.posX == x && d.posY == y)
-                    {
-                        isOccupied = true;
-                        break;
-                    }
-                }
-            } while (isOccupied);
+            // Configurer les attributs de la classe Dice
+            var diceComponent = dice.GetComponent<Dice>();
+            diceComponent.posX = x;
+            diceComponent.posY = y;       
+            diceComponent.isAI = isAI;   
+            diceComponent.place(x, y); 
 
-            // gérer les attributs de la classe Dice
-            dice.GetComponent<Dice>().posX = x;
-            dice.GetComponent<Dice>().posY = y;       
-            dice.GetComponent<Dice>().isAI = true;     
-
-            // Ajouter le dé à la liste des joueurs
-            dices.Add(dice.GetComponent<Dice>());
+            // Ajouter le dé à la liste des dés
+            dices.Add(diceComponent);
         }
-
-
-        view.PlaceDices(dices);
     }
 
 
-    // Génère une map 
-    // Affiche la map dans la vue
-    private void GenerateMap(int id = 1){
-        if (id == 1){
+    // Trouver une position aléatoire vide non occupée
+    private (int, int) FindEmptyPosition() {
+        int x, y;
+        bool isOccupied;
+        do {
+            x = Random.Range(0, map.GetLength(0));
+            y = Random.Range(0, map.GetLength(1));
+            isOccupied = !isFree(x, y);
+        } while (isOccupied);
 
-            map = new int[,]
-            {
-                {1, 1, 1, 0, 1, 1, 1, 0, 1, 1},
-                {1, 0, 1, 1, 1, 0, 1, 1, 1, 1},
-                {1, 1, 0, 1, 1, 1, 0, 1, 1, 1},
-                {1, 1, 1, 1, 0, 1, 1, 1, 0, 1},
-                {0, 1, 1, 1, 1, 1, 1, 0, 1, 1},
-                {1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
-                {1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
-                {1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
-                {1, 0, 1, 1, 1, 1, 1, 0, 1, 1},
-                {1, 1, 1, 1, 0, 1, 1, 1, 1, 1}
-            };
-        } else if (id == 2){
-            map = new int[,] {
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
-                {1, 0, 0, 1, 1, 1, 0, 0, 1, 1}, 
-                {1, 1, 0, 0, 1, 1, 0, 0, 1, 1}, 
-                {1, 1, 1, 0, 0, 1, 1, 1, 1, 1}, 
-                {1, 1, 1, 1, 0, 0, 1, 0, 0, 1}, 
-                {1, 0, 1, 1, 1, 1, 1, 0, 0, 1}, 
-                {1, 0, 0, 1, 1, 0, 1, 1, 1, 1}, 
-                {1, 1, 0, 0, 1, 1, 0, 1, 1, 1}, 
-                {1, 1, 1, 1, 1, 1, 0, 0, 1, 1}, 
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-            };
+        return (x, y);
+    }
+
+
+    // Génère une map et l'affiche
+    private void GenerateMap(){
+
+        map = new int[,]
+        {
+            {1, 1, 1, 0, 1, 1, 1, 0, 1, 1},
+            {1, 0, 1, 1, 1, 0, 1, 1, 1, 1},
+            {1, 1, 0, 1, 1, 1, 0, 1, 1, 1},
+            {1, 1, 1, 1, 0, 1, 1, 1, 0, 1},
+            {0, 1, 1, 1, 1, 1, 1, 0, 1, 1},
+            {1, 1, 0, 1, 1, 1, 1, 1, 1, 0},
+            {1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+            {1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+            {1, 0, 1, 1, 1, 1, 1, 0, 1, 1},
+            {1, 1, 1, 1, 0, 1, 1, 1, 1, 1}
+        };
+
+        // Placer les tiles
+        int mapX = map.GetLength(0);
+        int mapY = map.GetLength(1);
+        for (int x = 0; x < mapX; x++)
+        {
+            for (int y = 0; y < mapY; y++){
+                if (map[x,y] == 0) continue; // Si la case est vide
+                GameObject tile = tilePrefabs[Random.Range(0, tilePrefabs.Length)];
+                tile = Instantiate(tile, new Vector3(x, y , 0), Quaternion.identity);
+                tile.name = "Tile_" + x + "_" + y;
+                tile.transform.parent = mapContainer.transform;
+            }
         }
-
-        // Afficher la map dans la vue
-        view.PlaceTiles(map);
     }
 
 
 
-
+    // Vérifier si une case est vide et non occupée
     private bool isFree(int x, int y){
         // Vérifier si la case est dans la map
         if (x < 0 || x >= map.GetLength(0) || y < 0 || y >= map.GetLength(1))
@@ -197,84 +147,160 @@ public class GameManager : MonoBehaviour
 
         return true;
     }
-
+    
 
     void Update(){
-        CheckEnd();
-        if (endGame) return;
-        // check si un element de la liste est en missing ou null
-        for (int i = 0; i < dices.Count; i++)
+
+        // on left click
+        if (Input.GetMouseButtonDown(0))
         {
-            if (dices[i] == null)
+            Vector2 screenPosition = Input.mousePosition; // Position de la souris en coordonnées de l'écran
+            Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition); // Conversion en coordonnées du monde
+            RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
+            if (hit)
             {
-                dices.RemoveAt(i);
-                if (playerTurn >= dices.Count)
+                if (hit.transform.gameObject.tag == "Dice")
                 {
-                    playerTurn = 0;
+                    detailPanel.SetActive(true);
+                    Dice dice = hit.transform.gameObject.GetComponent<Dice>();
+                    detailPanel.transform.GetChild(0).GetComponent<Text>().text = dice.getDetails();
+
+                    // Conversion pour UI en utilisant Screen Space - Camera ou World Space
+                    RectTransform canvasRectTransform = detailPanel.GetComponentInParent<Canvas>().GetComponent<RectTransform>();
+                    Vector2 viewportPosition = Camera.main.ScreenToViewportPoint(screenPosition); // Conversion en position de viewport
+                    Vector2 uiPosition = new Vector2(
+                        ((viewportPosition.x * canvasRectTransform.sizeDelta.x) - (canvasRectTransform.sizeDelta.x * 0.5f)),
+                        ((viewportPosition.y * canvasRectTransform.sizeDelta.y) - (canvasRectTransform.sizeDelta.y * 0.5f))
+                    );
+                    detailPanel.transform.localPosition = uiPosition; // Appliquer la position transformée
                 }
-                return;
             }
         }
-
-
-        // GESTION DES TOURS
-        if (dices[playerTurn].isDead) {
-            return;
-        }
-
-        // lister les actions possibles du dices[playerTurn]
-        string[] actions = new string[8];
-        for (int i = 0; i < actions.Length; i++)
+        else if (Input.GetMouseButtonUp(0))
         {
-            actions[i] = "";
+            detailPanel.SetActive(false);
         }
-        int x = dices[playerTurn].posX;
-        int y = dices[playerTurn].posY;
-        if (isFree(x, y+1)) actions[0] = "0;1";
-        if (isFree(x+1, y+1)) actions[1] = "1;1";
-        if (isFree(x+1, y)) actions[2] = "1;0";
-        if (isFree(x+1, y-1)) actions[3] = "1;-1";
-        if (isFree(x, y-1)) actions[4] = "0;-1";
-        if (isFree(x-1, y-1)) actions[5] = "-1;-1";
-        if (isFree(x-1, y)) actions[6] = "-1;0";
-        if (isFree(x-1, y+1)) actions[7] = "-1;1";
 
-        if (dices[playerTurn].isAI) {
-            if (!dices[playerTurn].isTurn && !dices[playerTurn].hasPlayed){
-                dices[playerTurn].isTurn = true;
-                dices[playerTurn].turn(actions, dices);
-            } 
 
-            if (!dices[playerTurn].hasPlayed && dices[playerTurn].isTurn) return;
 
-            dices[playerTurn].hasPlayed = false;
-            dices[playerTurn].isTurn = false;
-            dices[playerTurn].makeEffect(dices);
-            playerTurn++;
+        CheckEnd();
+        if (endGame) return;
+
+        // Vérifier si on donne le tour à un dé qui a été tué
+        if (getDicePlaying() == null) {
+            // remove the element from the list
+            dices.RemoveAt(playerTurn);
             if (playerTurn >= dices.Count)
             {
                 playerTurn = 0;
             }
-            view.PlaceDices(dices);
-        } else {
-            // Si c'est le tour d'un joueur
-            
-            dices[playerTurn].isTurn = true;
-            dices[playerTurn].turn(actions);
+        }
+    
+        
 
-            if (dices[playerTurn].hasPlayed){
-                dices[playerTurn].makeEffect(dices);
-                dices[playerTurn].isTurn = false;
-                dices[playerTurn].hasPlayed = false;
+        switch (getDicePlaying().state)
+        {
+            case "":
+                getDicePlaying().eventIsTurn();
+                highlightTiles(getActions(getDicePlaying()));
+                break;
+
+            case "wait":
+                // Si c'est un dé IA, on applique la méthode iaTurn (calcul de l'action à faire) 
+                if (getDicePlaying().isAI) {
+                    getDicePlaying().iaTurn(getActions(getDicePlaying()), dices);
+                }
+                
+                // Si c'est un dé du joueur, on attend un clic
+                else {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+                        RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero);
+                        if (hit)
+                        {
+                            if (hit.transform.gameObject.tag == "Tile"){
+                                int clic_x, clic_y;
+                                string[] tmp = hit.transform.gameObject.name.Split('_');
+                                clic_x = int.Parse(tmp[1]);
+                                clic_y = int.Parse(tmp[2]);
+                                // if the tile is in the list of actions
+                                if (getActions(getDicePlaying()).Contains(clic_x + ";" + clic_y))
+                                {
+                                    StartCoroutine(getDicePlaying().moving(clic_x, clic_y));
+                                }
+                            }
+                        }
+                    }    
+                }
+
+            break;
+
+            case "moving":
+                removeHighlightTiles();
+            break;
+
+            case "effect":
+                getDicePlaying().makeEffect(dices);
+                break;
+
+            case "end":
+                getDicePlaying().state = "";
                 playerTurn++;
                 if (playerTurn >= dices.Count)
                 {
                     playerTurn = 0;
                 }
-                view.PlaceDices(dices);
-            }
+                break;
+
         }
     }
+        
+
+
+
+
+
+
+
+    // Retourne les cases où le dé peut se déplacer
+    private List<string> getActions(Dice dice){
+        int x = dice.posX;
+        int y = dice.posY;
+        List<string> actions = new List<string>();
+        if (isFree(x, y+1)) actions.Add(x + ";" + (y+1));
+        if (isFree(x+1, y+1)) actions.Add((x+1) + ";" + (y+1));
+        if (isFree(x+1, y)) actions.Add((x+1) + ";" + y);
+        if (isFree(x+1, y-1)) actions.Add((x+1) + ";" + (y-1));
+        if (isFree(x, y-1)) actions.Add(x + ";" + (y-1));
+        if (isFree(x-1, y-1)) actions.Add((x-1) + ";" + (y-1));
+        if (isFree(x-1, y)) actions.Add((x-1) + ";" + y);
+        if (isFree(x-1, y+1)) actions.Add((x-1) + ";" + (y+1));
+        return actions;        
+    }
+
+    // Affiche les cases où le dé peut se déplacer 
+    private void highlightTiles(List<string> actions){
+        foreach (string action in actions)
+        {
+            string[] tmp = action.Split(';');
+            int x = int.Parse(tmp[0]);
+            int y = int.Parse(tmp[1]);
+            GameObject tile = Instantiate(tileSurbrillance, new Vector3(x, y , 0), Quaternion.identity);
+            tile.name = "Tile_" + x + "_" + y;
+            tile.transform.parent = highlightTilesContainer.transform;
+        }
+    }
+
+    // Supprime la surbrillance des cases
+    private void removeHighlightTiles(){
+        foreach (Transform child in highlightTilesContainer.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+
 
 
 
@@ -282,7 +308,7 @@ public class GameManager : MonoBehaviour
         return dices[playerTurn];
     }
 
-
+    // Check si c'est la fin de la partie et affiche le panel de fin
     public void CheckEnd(){
         // Vérifier si un joueur a gagné
         int nbPlayerAlive = 0;
--- Assets/Scripts/Game/PlayerControler.cs
+++ Assets/Scripts/Game/PlayerControler.cs
@@ -19,32 +19,7 @@ public class PlayerControler : MonoBehaviour
     void Update()
     {
 
-        // On click on the mouse send a raycast and debug the hit
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Convertir la position de la souris en point dans le monde 2D
-            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero);
-
-            // Si le raycast touche quelque chose
-            if (hit)
-            {
-                if (hit.transform.gameObject.tag == "Tile"){
-                    int x, y;
-                    string[] tmp = hit.transform.gameObject.name.Split('_');
-                    x = int.Parse(tmp[1]);
-                    y = int.Parse(tmp[2]);
-                    Debug.Log("Tile : " + x + ";" + y);
-                    gameManager.getDicePlaying().lisenInput(x, y);
-                }
-
-                
-            } 
-            else 
-            {
-                Debug.Log("No hit");
-            }
-        }
+        
 
 
 
